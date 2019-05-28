1e3843a7e683d5ff5e4559fa35ae985309bd038e
Justas Birgiolas <JustasB@users.noreply.github.com>
2019-05-27 20:30:14 -0700

    Update __init__.py

--- linetimer/__init__.py
+++ linetimer/__init__.py
@@ -5,7 +5,8 @@ class CodeTimer:
     def __init__(self, name=None, silent=False):
         '''Allows giving indented blocks their own name. Blank by default'''
         self.name = name
-
+        self.silent = silent
+        
     def __enter__(self):
         '''Start measuring at the start of indent'''
         self.start = timeit.default_timer()
@@ -17,7 +18,7 @@ class CodeTimer:
         '''
         self.took = (timeit.default_timer() - self.start) * 1000.0
         
-        if not silent:
+        if not self.silent:
             print('Code block' + 
                   (" '"  + str(self.name) + "'" if self.name else '') + 
                   ' took: ' + str(self.took) + ' ms')
