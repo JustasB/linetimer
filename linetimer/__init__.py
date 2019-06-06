5ffddb66ff20b0f217a82b7ec67c67f9b1bc2e1d
Chitral Verma <chitralverma@gmail.com>
2019-06-06 14:44:03 +0530

    Formatting as per PEP8

--- linetimer/__init__.py
+++ linetimer/__init__.py
@@ -1,24 +1,25 @@
 import timeit
 
+
 class CodeTimer:
 
     def __init__(self, name=None, silent=False):
-        '''Allows giving indented blocks their own name. Blank by default'''
+        """Allows giving indented blocks their own name. Blank by default"""
         self.name = name
         self.silent = silent
-        
+
     def __enter__(self):
-        '''Start measuring at the start of indent'''
+        """Start measuring at the start of indent"""
         self.start = timeit.default_timer()
 
     def __exit__(self, exc_type, exc_value, traceback):
-        '''
-            Stop measuring at the end of indent. This will run even 
+        """
+            Stop measuring at the end of indent. This will run even
             if the indented lines raise an exception.
-        '''
+        """
         self.took = (timeit.default_timer() - self.start) * 1000.0
-        
+
         if not self.silent:
-            print('Code block' + 
-                  (" '"  + str(self.name) + "'" if self.name else '') + 
+            print('Code block' +
+                  (" '" + str(self.name) + "'" if self.name else '') +
                   ' took: ' + str(self.took) + ' ms')
